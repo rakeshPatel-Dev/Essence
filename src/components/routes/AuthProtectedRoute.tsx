343a979d8998588c1bb63327e3f1c449217e77ac
Rakesh Patel <rk5080976@gmail.com>
2026-01-24 19:29:01 +0545

    auth bug fixed

--- src/components/routes/AuthProtectedRoute.tsx
+++ src/components/routes/AuthProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../../context/authContext";
+import { useAuth } from "../../context/AuthContext";
 import type { JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
